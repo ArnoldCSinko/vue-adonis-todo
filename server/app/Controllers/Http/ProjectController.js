d6a0e6e53733fcbb83aa8299d3beabdf9b50ec1c
Chris Sinko <arnoldcsinko@gmail.com>
2018-06-02 17:43:43 -0400

    add Projects destroy Controller and delete Route

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -18,6 +18,17 @@ class ProjectController {
         await user.projects().save(project);
         return project;
     }
+
+    async destroy({ auth, request, params}) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        if(project.user_id !== user.id) {
+            return Response.status(403);
+        }
+        await project.delete();
+        return project;
+    }
 }
 
 module.exports = ProjectController
